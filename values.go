f6898ea8fe5489c1e474cfd842737631dfe2799b
Sameera Dissanayaka <sameerad177@gmail.com>
2019-12-04 18:16:15 +0530

    values in go

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("2+2 is", 2+2)
+	fmt.Println("7.0/2.0 is", 7.0/2.0)
+	fmt.Println("7.0/2 is", 7/2)
+	fmt.Println("my name is", "Sameera")
+	fmt.Println("true and false is ", true && false)
+	fmt.Println("true or false is", true || false)
+	fmt.Println("not ture is", !true)
+
+}
